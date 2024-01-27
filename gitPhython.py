6881e3b2bd14e0499073f1231b205835089872b6
StephenG89 <76877783+StephenG89@users.noreply.github.com>
2024-01-27 17:10:34 -0500

    Update gitPhython.py

--- gitPhython.py
+++ gitPhython.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import time
 
 # Set the path to your MP3 file
-file_path = "gitRepos/test/Prelude.mp3"
+file_path = "./Prelude.mp3"
 
 # Set the GPIO pin for controlling audio
 audio_control_pin = 18  # Replace with the actual GPIO pin number
